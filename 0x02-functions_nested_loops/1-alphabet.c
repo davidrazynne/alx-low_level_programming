fd75bfbff5dbd3f6f686a1f19573e4e2f5547260
davidrazynne <davidrazynne@gmail.com>
2023-07-18 13:56:39 -0700

    my second task of list of a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet chaaracters in lowercase
+*
+* Return:void
+*/
+
+void print_alphabet(void)
+{
+char cha;
+
+for (cha = 'a'; cha <= 'z'; cha++)
+_putchar(cha);
+
+_putchar('\n');
+}
+
