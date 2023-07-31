72c6a1e6769ceb41a8490e643cc5435aa8c2def1
davidrazynne <davidrazynne@gmail.com>
2023-07-31 15:04:31 -0700

    question 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,24 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - a function that sets the value of a pointer to a char
- * @a: the matrix of the integers
- * @size: size of matrix
- * Return: Always 0
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
 {
-int index, sum1 = 0, sum2 = 0;
-for (index = 0; index < size; index++)
-{
-sum1 += a[index];
-a += size;
-}
-a += size;
-for (index = 0; index < size; index++)
-{
-sum2 += a[index];
-a -= size;
-}
-printf("%d, %d\n", sum1, sum2);
+	int sum1, sum2, y;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum1 + a[y * size + y];
+	}
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
