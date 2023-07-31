27a77c4a199b86f97f74e3c785f0f5a3ad3f2b91
davidrazynne <davidrazynne@gmail.com>
2023-07-31 13:49:32 -0700

    Chess real is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
  * print_chessboard - a function that prints the chessboard.
- * @a: array
- * Return: Always 0
+ * @a: pointer to pieces to print
+ * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
-        int i;
-        int j;
-        for (i = 0; i < 8; i++)
-        {
-                for (j = 0; j < 8; j++)
-		{
-                        _putchar(a[i][j]);
-		}
-                _putchar('\n');
-        }
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
