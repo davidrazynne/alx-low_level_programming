7e37baa143ce6f7d1a70ea934be6759100409a3f
davidrazynne <davidrazynne@gmail.com>
2023-07-31 06:17:39 -0700

    a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN H
-#define MAIN H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar
 char *_memset(char *s, char b, unsigned int n);
@@ -12,3 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
 #endif
+
