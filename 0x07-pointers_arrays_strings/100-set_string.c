4ebf3aabd6a7053524b79b1c7e11a9aa0c1a9d53
davidrazynne <davidrazynne@gmail.com>
2023-07-31 14:27:49 -0700

    Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -8,18 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
-
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-	printf("%d, %d\n", sum1, sum2);
+int index, sum1 = 0, sum2 = 0;
+for (index = 0; index < size; index++)
+{
+sum1 += a[index];
+a += size;
+}
+a += size;
+for (index = 0; index < size; index++)
+{
+sum2 += a[index];
+a -= size;
+}
+printf("%d, %d\n", sum1, sum2);
 }
