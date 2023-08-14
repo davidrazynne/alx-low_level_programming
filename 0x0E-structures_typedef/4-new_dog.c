1ab76724b0db765194997ea478c73430ad113f09
davidrazynne <davidrazynne@gmail.com>
2023-08-14 21:22:41 +0300

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,3 +21,74 @@ int _strlen(char *s)
 
 	return (i);
 }
+/**
+ * *_strcpy - this function copies the string pointed to by src
+ *
+ * which will include a terminating null byte: backslash zero
+ *
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: this indicates that string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - this fxn creates a new dog
+ * @age: this indicates age of the dog
+ * @owner: this indicates owner of the dog
+ *
+ * @name: this indicates name of the dog
+ *
+ * Return: a pointer to the new dog (Success), NULL is the alternative.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
+
