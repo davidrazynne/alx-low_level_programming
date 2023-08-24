f904db854ec83be8ba07dc01eda96008ca91be49
davidrazynne <davidrazynne@gmail.com>
2023-08-24 20:54:44 +0300

    zero

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,20 @@
  */
 size_t print_list(const list_t *h)
 {
-        int count = 0;
+int count = 0;
 
-	while (h)
-	{
-		if (!h->str)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
-		h = h->next;
-    	}
-
-	return (count);
+while (h)
+{
+if (!h->str)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+count++;
+h = h->next;
+}
+return (count);
 }
