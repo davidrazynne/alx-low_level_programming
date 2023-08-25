7bf0cf48713a4c7d29f089f110becf13a3056efc
davidrazynne <davidrazynne@gmail.com>
2023-08-25 20:49:47 +0300

    correction

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,9 @@
-section .data
-    hello_message db "Hello, Holberton", 0
-    format db "%s", 10, 0
-
-section .bss
-    rsp_save resq 1
-
-section .text
-    global main
-    extern printf
-
+global   main
+          extern    printf
 main:
-    mov [rsp_save], rsp
-
-    mov rdi, format
-    mov rsi, hello_message
-    call printf
-
-    mov rsp, qword [rsp_save]
-
-    mov rax, 60
-    xor rdi, rdi
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov   eax, 0
+          ret
+format: db "Hello, Holberton\n",0
