9b43b363a20ac596a90abf27896c42e77c01410c
davidrazynne <davidrazynne@gmail.com>
2023-08-29 20:02:33 +0300

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: type listint_t list to roam
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
