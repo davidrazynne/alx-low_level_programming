644cb275fbfde2b5faa7ef0fecd1aba79b50a78c
davidrazynne <davidrazynne@gmail.com>
2023-08-30 21:04:04 +0300

    101 not complet

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * print_listint_safe - Prints a safe version of listint_t list
+ * @head: An address to the 1st node of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+
