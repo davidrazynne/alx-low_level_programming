6bbc9652507b9b3c4c79aca10578ac2bda736e6b
davidrazynne <davidrazynne@gmail.com>
2023-09-04 20:35:11 +0300

    a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: NULL terminated string to write to the file
  *
  * Return:  1 if successful or -1 if it fails
- *         
+ *
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -31,4 +31,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
