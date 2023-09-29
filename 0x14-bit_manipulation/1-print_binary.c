e99f4c481fc956544e77822639e6f7c54606e678
davidrazynne <davidrazynne@gmail.com>
2023-09-29 19:04:06 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,23 @@
  * @n: the actual no to print in binary form
  *
  */
+
 void print_binary(unsigned long int n)
 {
-        int e, count = 0;
-        unsigned long int current;
+int e, count = 0;
+unsigned long int current;
 
-        for (e = 63; e >= 0; e--)
-        {
-                current = n >> e;
-
-                if (current & 1)
-                {
-                        _putchar('1');
-                        count++;
-                }
-                else if (count)
-                        _putchar('0');
-        }
-        if (!count)
-                _putchar('0');
+for (e = 63; e >= 0; e--)
+{
+current = n >> e;
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
 }
