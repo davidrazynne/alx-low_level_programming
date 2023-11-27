1090e2e7cfc1b6ef78ba7c18c53530b9c04ffd18
davidrazynne <davidrazynne@gmail.com>
2023-11-27 23:34:21 +0300

    program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * append_text_to_file - appends text at the end of a file.
+ * append_text_to_file - appends text at the end of a file
  * @filename: points to the name of the file
  * @text_content: terminated string to add at the end of the file
  * Return: -1(function fails, user lacks write permisssons or does not exist
@@ -9,19 +9,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
-o - open(filename, O_WRONLY | O_APPEND);
-w = write(o, text_content, len);
-	if (o == -1 || w == -1)
-		return (-1);
-	close(o);
+
+	close(fd);
+
 	return (1);
 }
